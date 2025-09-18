6e4523044851d3b66f1288d94c598b27cefddd7a
ELFASA <elfasatasa@gmail.com>
2025-09-18 05:42:28 +0500

    qweqweqwe

--- src/components/AddStandart/AddStandart.tsx
+++ src/components/AddStandart/AddStandart.tsx
@@ -1,17 +1,17 @@
 // src/components/AddStandart/AddStandart.tsx
-
+'use client'
 import { Default } from '@/types/default'; // Импортируем типы
 import styles from './AddStandart.module.scss';
 
 import { useState } from 'react';
 
-
+import MathTextarea from '../MathInput/MathInput';
 
 
 
 export default function AddStandart({ onChange, questions }: Default) {
 
-
+ const [latex, setLatex] = useState("f(x)=\\log _10 x");
   return (
     <div className={styles.container}>
       <div className={styles.title}>
@@ -19,9 +19,8 @@ export default function AddStandart({ onChange, questions }: Default) {
       </div>
 
       <br />
-\
-
-
+      <MathTextarea value={latex} onChange={setLatex} />
+      <p className="mt-4">LaTeX: {latex}</p>
    
     </div>
   );
